fa3fa4f83e101b97d6a29d4a153b829d671aa86b
Hans <hans870709@gmail.com>
2020-02-14 01:27:11 +0900

    0

--- src/init.js
+++ src/init.js
@@ -6,16 +6,11 @@ const PORT = 4000;
 const handleListening = () =>
     console.log(`Listening on: http://localhost:${PORT}`);
 
-
 const handleHome = (req, res) => res.send("Hello from home!!");
-
-
 const handleProfile = (req, res) => res.send("You are on my profile");
 
 
 
 app.get("/", handleHome);
-
 app.get("/profile", handleProfile);
-
 app.listen(PORT, handleListening);
