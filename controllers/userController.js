adfca193cbfd2860eb2a4a00f59028bff3132030
Hans <hans870709@gmail.com>
2020-02-16 19:40:01 +0900

    Seach Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
-export const userDetail = (req, res) => res.render("userDetail");
-export const editProfile = (req, res) => res.render("editProfile");
-export const changePassword = (req, res) => res.render("changePassword");
+export const join = (req, res) => res.render("join", {pageTile: "Join" });
+export const login = (req, res) => res.render("login", {pageTile: "Login" });
+export const logout = (req, res) => res.render("logout", {pageTile: "Logout" });
+export const userDetail = (req, res) => res.render("userDetail", {pageTile: "User Detail" });
+export const editProfile = (req, res) => res.render("editProfile", {pageTile: "Edit Profile" });
+export const changePassword = (req, res) => res.render("changePassword", {pageTile: "Change Password" });
