d64acc60350ddafd27153ce69e441307fc736c86
Hans <hans870709@gmail.com>
2020-02-17 19:46:41 +0900

    Home Controller

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+    {
+        id:324393,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://www.youtube.com/embed/F9Bo89m2f6g",
+        creator: {
+            id: 797979,
+            name: "Hans",
+            email: "Hans@lets.co"
+        }
+    },
+    {
+        id:12322,
+        title: 'Video Super',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://www.youtube.com/embed/F9Bo89m2f6g",
+        creator: {
+            id: 797979,
+            name: "Hans",
+            email: "Hans@lets.co"
+        }
+    },
+    {
+        id:878787,
+        title: 'Video Good',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "https://www.youtube.com/embed/F9Bo89m2f6g",
+        creator: {
+            id: 797979,
+            name: "Hans",
+            email: "Hans@lets.co"
+        }
+    }
+];
