35ae33f7254880772c08a1c6f35d234f4a4a723a
Hans <hans870709@gmail.com>
2020-03-06 15:30:29 +0800

    Github login passport one

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
   entry: ["@babel/polyfill", ENTRY_FILE],
-  mode: MODE,
+  mode: MODE.replace(/\s/g, ""),
   module: {
     rules: [
       {
