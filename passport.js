80c6961ee0b33e99b3ca88f8c662401c7b0a7cfe
Hans <hans870709@gmail.com>
2020-03-07 15:19:22 +0800

    User Profile

--- passport.js
+++ passport.js
@@ -26,9 +26,7 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `https://afraid-baboon-46.localtunnel.me${
-        routes.facebookCallback
-      }`,
+      callbackURL: `https://afraid-baboon-46.localtunnel.me${routes.facebookCallback}`,
       profileFields: ["id", "displayName", "photos", "email"],
       scope: ["public_profile", "email"]
     },
